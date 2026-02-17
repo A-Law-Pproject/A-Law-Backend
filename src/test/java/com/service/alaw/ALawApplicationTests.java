0f1469d0324b731db25aac1259bfdff9cfd3ecfc
hwangjiyoung02 <hjy020714@gmail.com>
2026-02-18 00:33:05 +0900

    🔥fix: test 환경용 application-test.yml 설정

--- src/test/java/com/service/alaw/ALawApplicationTests.java
+++ src/test/java/com/service/alaw/ALawApplicationTests.java
@@ -2,8 +2,10 @@ package com.service.alaw;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ALawApplicationTests {
 
     @Test
