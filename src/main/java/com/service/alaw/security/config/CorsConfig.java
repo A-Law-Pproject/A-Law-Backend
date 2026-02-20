4f3e1db66fc166747f4216144a822a97bb752536
hwangjiyoung02 <hjy020714@gmail.com>
2026-02-20 14:44:08 +0900

    🔥 fix: CorsConfig 수정

--- src/main/java/com/service/alaw/security/config/CorsConfig.java
+++ src/main/java/com/service/alaw/security/config/CorsConfig.java
@@ -22,6 +22,9 @@ public class CorsConfig {
     @Value("${cors.back.dev}")
     private String back_dev;
 
+    @Value("${cors.back.prod}")
+    private String back_prod;
+
     /**
      * CORS 설정을 진행합니다.
      */
@@ -34,6 +37,7 @@ public class CorsConfig {
         configuration.addAllowedOriginPattern(front_dev);
         configuration.addAllowedOriginPattern(front_prod);
         configuration.addAllowedOriginPattern(back_dev);
+        configuration.addAllowedOriginPattern(back_prod);
 
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
