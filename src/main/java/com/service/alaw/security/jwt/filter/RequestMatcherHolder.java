4a5f2be56c5f02b9af61797cc78e7e6f3fffcf48
hwangjiyoung02 <hjy020714@gmail.com>
2026-02-20 14:29:12 +0900

    🔥 fix: swagger 경로 수정

--- src/main/java/com/service/alaw/security/jwt/filter/RequestMatcherHolder.java
+++ src/main/java/com/service/alaw/security/jwt/filter/RequestMatcherHolder.java
@@ -55,8 +55,7 @@ public class RequestMatcherHolder {
             new RequestInfo(GET, "/actuator/**", null),
 
             // Swagger UI 및 API 문서
-            new RequestInfo(GET, "/api/v1/api-docs/**", null),
-            new RequestInfo(GET, "/api/v1/swagger-ui/**", null),
+            new RequestInfo(GET, "/v3/api-docs/**", null),
             new RequestInfo(GET, "/swagger-ui/**", null),
             new RequestInfo(GET, "/swagger-resources/**", null),
             new RequestInfo(GET, "/webjars/**", null),
